391e105898738db15f7d8e398e2101e3afa89321
tanvu <tdv@princeton.edu>
2021-06-14 10:51:31 -0400

    Minify assests

--- js/scripts.js
+++ js/scripts.js
@@ -3,47 +3,6 @@
  * Copyright 2013-2021 Start Bootstrap
  * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
  */
-//
-// Scripts
-// 
-
-window.addEventListener('DOMContentLoaded', event => {
-
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
-
-    };
-
-    // Shrink the navbar 
-    if (document.body.querySelector('#blog-list') || document.body.querySelector('#blog-post')) {
-    }
-    else {
-        navbarShrink();
-        // Shrink the navbar when page is scrolled
-        document.addEventListener('scroll', navbarShrink);
-        console.log("asdfasdf");
-    }
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-
-});
+window.addEventListener('DOMContentLoaded',event=>{var navbarShrink=function(){const navbarCollapsible=document.body.querySelector('#mainNav');if(!navbarCollapsible){return}
+if(window.scrollY===0){navbarCollapsible.classList.remove('navbar-shrink')}else{navbarCollapsible.classList.add('navbar-shrink')}};if(document.body.querySelector('#blog-list')||document.body.querySelector('#blog-post')||document.body.querySelector('#guide-onboard')){}else{navbarShrink();document.addEventListener('scroll',navbarShrink);console.log("asdfasdf")}
+const navbarToggler=document.body.querySelector('.navbar-toggler');const responsiveNavItems=[].slice.call(document.querySelectorAll('#navbarResponsive .nav-link'));responsiveNavItems.map(function(responsiveNavItem){responsiveNavItem.addEventListener('click',()=>{if(window.getComputedStyle(navbarToggler).display!=='none'){navbarToggler.click()}})})})
